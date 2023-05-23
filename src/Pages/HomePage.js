bf84dcff5217310905503b586bb072f25e9b8b19
Nikiden6 <ol.daniluyk@gmail.com>
2023-05-23 22:35:39 +0100

    hgf

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to My Online Shop</h1>
+        </header>
+        <main>
+          {/* Add content like featured products, categories, etc. */}
+        </main>
+        <footer>
+          {/* Add footer content */}
+        </footer>
+      </div>
+    );
+  }
---
+++ src/Pages/HotDealsPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HotDeals = () => {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to HotDeals</h1>
+        </header>
+        <main>
+          {/* Add content like featured products, categories, etc. */}
+        </main>
+        <footer>
+          {/* Add footer content */}
+        </footer>
+      </div>
+    );
+  }
