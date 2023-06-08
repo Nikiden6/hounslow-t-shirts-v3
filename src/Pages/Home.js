e1c9506879fab352958c4fe25c597e7a7c9c4b13
Oleg Daniliuc <40163200@hcuc.ac.uk>
2023-06-08 12:28:58 +0100

    home page css

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import h_img_1 from '../data/HomePageImg/h_img_1.png';
+import h_img_2 from '../data/HomePageImg/h_img_2.png';
+import '../Pages/Home.css'
 
 const Home = () => {
     return (
-      <div>
+      <div className='HomePage'>
         <header>
           <h1>Welcome to My Online Shop</h1>
         </header>
         <main>
           {/* Add content like featured products, etc. */}
+          <section>
+            <h2>Buy 2 our best t-shirt and get 1 free </h2>
+            <img src= {h_img_1}/>
+            <img src= {h_img_2}/>
+
+          </section>
         </main>
         <footer>
           {/* Add footer content */}
