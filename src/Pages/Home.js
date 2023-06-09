91ef7614bb0d15e3f5a068c267740d188cfe3c15
Oleg Daniliuc <40163200@hcuc.ac.uk>
2023-06-09 12:56:38 +0100

    button advert correction

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import h_img_1 from '../data/HomePageImg/h_img_1.png';
 import h_img_2 from '../data/HomePageImg/h_img_2.png';
 import '../Pages/Home.css'
+import {Link} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -11,12 +13,20 @@ const Home = () => {
         </header>
         <main>
           {/* Add content like featured products, etc. */}
-          <section>
-            <h2>Buy 2 our best t-shirt and get 1 free </h2>
-            <img src= {h_img_1}/>
-            <img src= {h_img_2}/>
-
-          </section>
+          <div className='AdvertHome'>
+            <div className='ButtonShopNow'>
+              <h2>Buy 2 our best t-shirt and get 1 free </h2>
+                <button> 
+                  <Link to = '/best-sellings'>
+                    SHOP NOW
+                  </Link>
+                </button>  
+                <div className='AdvertImg'>
+                  <img src= {h_img_1}/>
+                  <img src= {h_img_2}/>
+              </div>
+            </div>
+          </div>
         </main>
         <footer>
           {/* Add footer content */}
