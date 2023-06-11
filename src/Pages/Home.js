3661df658e5a3adcad51c2e17d172ef91597e821
Nikiden6 <ol.daniluyk@gmail.com>
2023-06-11 13:29:28 +0100

    ...

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,27 +3,24 @@ import h_img_1 from '../data/HomePageImg/h_img_1.png';
 import h_img_2 from '../data/HomePageImg/h_img_2.png';
 import '../Pages/Home.css'
 import {Link} from 'react-router-dom'
-import { BrowserRouter } from 'react-router-dom';
 
 const Home = () => {
     return (
       <div className='HomePage'>
-        <header>
-          <h1>Welcome to My Online Shop</h1>
+        <header className='HomeHeader'>
+          <h1 className='Welcome'>Hounslow 3-D T-Shirts</h1>
         </header>
-        <main>
+        <main className='HomeBody'>
           {/* Add content like featured products, etc. */}
           <div className='AdvertHome'>
-            <div className='ButtonShopNow'>
-              <h2>Buy 2 our best t-shirt and get 1 free </h2>
-                <button> 
-                  <Link to = '/best-sellings'>
-                    SHOP NOW
-                  </Link>
-                </button>  
-                <div className='AdvertImg'>
-                  <img src= {h_img_1}/>
-                  <img src= {h_img_2}/>
+            <div className='SectionShopNow'>
+              <h2 className='BuyText'>Buy 2 our best t-shirt and get 1 free </h2>
+              <Link classsName = 'ShopNowButton' to = '/best-sellings'>
+                SHOP NOW
+              </Link>
+              <div className='AdvertImg'>
+                <img src= {h_img_1} alt = ""/>
+                <img src= {h_img_2}/>
               </div>
             </div>
           </div>
